d7ab1dbaa9453a3eb1f4d0be45889505b8000867
JuanCastillo18 <juandi18c@hotmail.com>
2025-11-05 19:15:23 +0100

    Update run.php

--- deaw/run.php
+++ deaw/run.php
@@ -1,11 +1,12 @@
 <?php
 
-
+// Activar reporte completo de errores para desarrollo
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Cargar configuración y funciones principales
 require __DIR__ . '/includes/config.php';
 require __DIR__ . '/includes/functions.php';
 
-
+// Inicializar la plantilla y mostrar la página
 init();
